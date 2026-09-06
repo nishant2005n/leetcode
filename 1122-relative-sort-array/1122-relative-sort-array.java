5442b190b79ac7a7a2d6c6115554b29737ac1661
nishant2005n <nishant.kumar2.cs28@iilm.edu>
2026-09-06 15:49:19 +0530

    Time: 5 ms (26.91%), Space: 44.1 MB (21.14%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int i = 0;
+       
+        for (int k = 0; k < arr2.length ; k++){
+            for(int j = 0; j < arr1.length ; j++){
+                if(arr2[k] == arr1[j]){
+                    int temp = arr1[i]; arr1[i] = arr1[j]; arr1[j] = temp;
+                    i++;
+                }
+                
+            }
+        }
+          Arrays.sort(arr1, i, arr1.length);
+      
+       return arr1;
+    
+        
+    }
+}
