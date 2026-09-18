6e07b6d5b1fdf116a2da51ab2b527bf4bfb7b431
nishant2005n <nishant.kumar2.cs28@iilm.edu>
2026-09-18 17:27:11 +0530

    Time: 3 ms (56.09%), Space: 44.7 MB (43.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
         int i = 0;
         int j = s.length () -1;
         while(i<j){
@@ -9,7 +10,7 @@ class Solution {
             while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
                 j--;
             }
-            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)) ){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
             i++;
